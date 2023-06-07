5ba5a0d7259d30e1b3d1b65c46006ab75b742985
qwertzuiii <78412852+qwertzuiii@users.noreply.github.com>
2023-06-07 16:01:29 +0200

    Better use update
    Moved the `__theme_change()` function out of the gui, so you can paste this function in your code and use it easier

--- gui.py
+++ gui.py
@@ -7,7 +7,8 @@ from tomllib import loads as tml
 from json import loads as jsn
 
 # Debug mode
-is_Debug = True
+print_Theme_Debug = False
+file_Theme_Debug = False
 #
 
 # Loading config
@@ -21,6 +22,48 @@ theme_folder = 'bin/themes/'
 theme_list = open(theme_folder + 'list.json', 'r').read()
 theme_list = jsn(theme_list)
 #
+
+def _convert_stylesheet(themeindex):
+    # Loading theme stylesheet
+    f   = theme_list[themeindex]['file']
+    vf  = theme_list[themeindex]['varfile']
+    n   = theme_list[themeindex]['name']
+    opt = theme_list[themeindex]['opt']
+
+    if opt == "":
+        o = False
+    else:
+        o = True
+    
+    if print_Theme_Debug:
+        print('Loading Theme: ' + n + " [{}]".format(f))
+        if o:
+            print('OPT: {}'.format(opt))
+
+    varsf = open(theme_folder + vf, 'r').read()
+    varsf = jsn(varsf)
+
+    style = open(theme_folder + f, 'r').read()
+
+    if o:
+        style_opt = open(theme_folder + opt, 'r').read()
+    
+    for var in varsf: # Replacing vars in variable file
+        style = style.replace(var, varsf[var])
+
+        if o:
+            style_opt = style_opt.replace(var, varsf[var])
+
+    if file_Theme_Debug:
+        if o:
+            open('STYLE.RESULT.--debug.css', 'w').write(style + "\n" + style_opt)
+        else:
+            open('STYLE.RESULT.--debug.css', 'w').write(style)
+
+    if o:
+        return style + "\n" + style_opt
+    else:
+        return style
 
 class MainApp(QMainWindow, QWidget):
     def __init__(self):
@@ -38,59 +81,18 @@ class MainApp(QMainWindow, QWidget):
     def __theme_list_refresh(self):
         themes = theme_list
         themes_len = len(themes)
-        if is_Debug: print(themes_len)
-        i = 0
+        if print_Theme_Debug: print(themes_len)
 
         for theme in range(themes_len):
-            self.ThemeBox.addItem(themes[i]['name'])
-            i += 1
+            self.ThemeBox.addItem(themes[theme]['name'])
 
     def __theme_change_box(self, value):
         self.theme_index = value
         self.__theme_change()
 
     def __theme_change(self):
-        # Loading theme stylesheet
-        f   = theme_list[self.theme_index]['file']
-        vf  = theme_list[self.theme_index]['varfile']
-        n   = theme_list[self.theme_index]['name']
-        opt = theme_list[self.theme_index]['opt']
-
-        if opt == "":
-            o = False
-        else:
-            o = True
+        self.setStyleSheet(_convert_stylesheet(self.theme_index))
         
-        print('Loading Theme: ' + n + " [{}]".format(f))
-        if o:
-            print('OPT: {}'.format(opt))
-
-        varsf = open(theme_folder + vf, 'r').read()
-        varsf = jsn(varsf)
-
-        style = open(theme_folder + f, 'r').read()
-
-        if o:
-            style_opt = open(theme_folder + opt, 'r').read()
-        
-        for var in varsf: # Replacing vars in variable file
-            style = style.replace(var, varsf[var])
-
-            if o:
-                style_opt = style_opt.replace(var, varsf[var])
-
-        if is_Debug:
-            if o:
-                open('STYLE.RESULT.--debug.css', 'w').write(style + "\n" + style_opt)
-            else:
-                open('STYLE.RESULT.--debug.css', 'w').write(style)
-
-        if o:
-            self.setStyleSheet(style + "\n" + style_opt)
-        else:
-            self.setStyleSheet(style)
-        
-        pass
 
 
 if __name__ == '__main__':
